bc252e1f0d6cfdce6dfbf3886d6848cb968a48a6
SimonUlbra <luissimon96@rede.ulbra.br>
2021-11-19 01:43:09 -0300

    Salvando trabalhando com números e criando uma caixa de pesquisa

---
+++ python_do_zero/numeros.py
@@ -0,0 +1,19 @@
+print(4)
+print(type(4))
+
+
+print('-----------------')
+
+print(14.70)
+print(type(14.70))
+
+print('=======================================')
+print('Numeros apartir das funcoes int e float')
+print('=======================================', end='\n\n')
+
+preco_ingredientes = 14.70
+print('Preco do Ingreditente Float: ',
+      preco_ingredientes, type(preco_ingredientes))
+
+print('Preco do Ingreditente int: ',
+      int(preco_ingredientes), type(int(preco_ingredientes)))
